935aa006d08c0753f4a7512103808dfdfb5bc4ff
My Digital Space <171423533+my-digital-space@users.noreply.github.com>
2026-05-10 13:14:20 +0530

    initial commit

---
+++ JWT_Auth/src/main/java/com/my/jwt/JwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.my.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthApplication.class, args);
+	}
+
+}
